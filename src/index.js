54987a7540042b91ddb560183cd89827ec373225
Adrian Iuliu Moldovan <adrianiuliu.moldovan@yahoo.com>
2023-07-07 21:30:28 +0300

    updateTeamRequest

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
 
+let allTeams = [];
+let editID;
+
 function $(selector) {
   return document.querySelector(selector);
 }
@@ -24,22 +27,54 @@ function deleteTeamRequest(id) {
   }).then(r => r.json());
 }
 
-function getTeamAsHTML(team) {
-  return `<tr>
-            <td>${team.promotion}</td>
-            <td>${team.members}</td>
-            <td>${team.name}</td>
-            <td>${team.url}</td>
-            <td>
-            <a data-id="${team.id}" class="delete-btn"> ✖ </a>
-            <a data-id="${team.id}" class="edit-btn"> &#9998; </a>
-
-            </td>
-        </tr>`;
+function updateTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  }).then(r => r.json());
 }
 
-function renderTeams(teams) {
-  const htmlTeams = teams.map(getTeamAsHTML);
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>
+    <a data-id="${team.id}" class="edit-btn"> &#9998; </a>
+    <a data-id="${team.id}" class="delete-btn"> ✖ </a>
+    </td>
+  </tr>`;
+}
+
+function getTeamAsHTMLInputs(team) {
+  return `<tr>
+    <td>
+      <input value="${team.promotion}" type="text" name="promotion" placeholder="Enter Promotion" />
+    </td>
+    <td>
+      <input value="${team.members}" type="text" name="members" placeholder="Enter Members" />
+    </td>
+    <td>
+      <input value="${team.name}" type="text" name="name" placeholder="Enter Project Name" />
+    </td>
+    <td>
+      <input value="${team.url}" type="text" name="url" placeholder="Enter Project URL" />
+    </td>
+    <td>
+      <button title="Save" type="submit">💾</button>
+      <button title="Cancel" type="reset">❌</button>
+    </td>
+  </tr>`;
+}
+
+function renderTeams(teams, editID) {
+  const htmlTeams = teams.map(team => {
+    return team.id === editID ? getTeamAsHTMLInputs(team) : getTeamAsHTML(team);
+  });
   //   console.warn(htmlTeams);
   $("#teamsTable tbody").innerHTML = htmlTeams.join("");
 }
@@ -47,31 +82,69 @@ function renderTeams(teams) {
 function loadTeams() {
   fetch("http://localhost:3000/teams-json")
     .then(r => r.json())
-    .then(renderTeams);
+    .then(teams => {
+      allTeams = teams;
+      renderTeams(teams);
+    });
+}
+
+function getTeamValues(parent) {
+  const promotion = $(`${parent} input[name=promotion]`).value;
+  const members = $(`${parent} input[name=members]`).value;
+  const name = $(`${parent} input[name=name]`).value;
+  const url = $(`${parent} input[name=url]`).value;
+  const team = {
+    promotion: promotion,
+    members: members,
+    name,
+    url
+  };
+  return team;
 }
 
 function onSubmit(e) {
   //   console.warn("submit", e);
   e.preventDefault();
 
-  const members = $("#members").value;
-  const name = $("input[name=name]").value;
-  const url = $("input[name=url]").value;
-  const team = {
-    promotion: $("#promotion").value,
-    members: members,
-    name: name,
-    url: url
-  };
+  const team = getTeamValues(editID ? "tbody" : "tfoot");
+
+  console.warn("update or create?", editID);
 
   console.warn(team);
 
-  createTeamRequest(team).then(status => {
-    console.warn("created", status);
-    if (status.success) {
-      window.location.reload();
-    }
+  if (editID) {
+    team.id = editID;
+    console.warn("update...", team);
+    updateTeamRequest(team).then(status => {
+      console.warn("updated", status);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  } else {
+    createTeamRequest(team).then(status => {
+      console.warn("created", status);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  }
+}
+
+function startEdit(id) {
+  editID = id;
+  console.warn("edit... %o", id);
+  // const team = allTeams.find(team => team.id === id);
+  renderTeams(allTeams, id);
+  document.querySelectorAll("tfoot input").forEach(input => {
+    input.disabled = true;
   });
+
+  // console.warn(team);
+  // $("#promotion").value = team.promotion;
+  // $("#members").value = team.members;
+  // // $("#name").value = team.name;
+  // // $("#url").value = team.url;
 }
 
 function initEvents() {
@@ -87,6 +160,9 @@ function initEvents() {
           window.location.reload();
         }
       });
+    } else if (e.target.matches("a.edit-btn")) {
+      const id = e.target.dataset.id;
+      startEdit(id);
     }
   });
 }
